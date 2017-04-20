cca8dbab8e19437dba8dd2c3ff59e7375f7297cb
Phillip Johnston <phillip.johnston@gatech.edu>
2017-04-20 09:08:39 -0700

    Cleanup malloc.h: add malloc_addblock to malloc.h, remove malloc_freelist.h. We will have multiple mallocs that need an addblock

--- src/malloc.h
+++ src/malloc.h
@@ -1,11 +1,14 @@
 #ifndef __MALLOC_H_
 #define __MALLOC_H_
 
-#include "malloc_freelist.h"
-
 #ifdef __cplusplus
 extern "C" {
 #endif //__cplusplus
+
+/**
+* Initialize malloc with a memory address and pool size
+*/
+void malloc_addblock(void *addr, size_t size);
 
 void * malloc(size_t size);
 void free(void * ptr);
