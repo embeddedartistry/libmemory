3268c136e5915f4aa8ad5dda24841c65d5e4d718
Phillip Johnston <phillip@embeddedartistry.com>
2018-09-06 13:17:07 -0700

    Add include for dependencies lib

--- tools/cppcheck_xml.sh
+++ tools/cppcheck_xml.sh
@@ -5,5 +5,6 @@ MESON_BUILD_ROOT=${MESON_BUILD_ROOT:-buildresults}
 
 cppcheck --quiet --enable=style --force --xml --xml-version=2 \
 	-I ${MESON_CURRENT_SOURCE_DIR}/include \
+	-I ${MESON_CURRENT_SOURCE_DIR}/dependencies/lib \
 	src test \
 	2>${MESON_BUILD_ROOT}/cppcheck.xml
