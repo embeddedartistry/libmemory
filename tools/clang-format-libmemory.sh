0417b60a1b32862d7d7d7a0ccced82801838f895
Phillip Johnston <phillip@embeddedartistry.com>
2020-05-07 09:37:00 -0700

    Remove scripts in preparation for new build system

--- tools/clang-format-libmemory.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-find . -type d \( -path ./dependencies/ \) -prune \
-	-iname *.h -o -iname *.c -o -iname *.cpp -o -iname *.hpp \
-	| xargs clang-format -style=file -i -fallback-style=none
-
-exit 0
